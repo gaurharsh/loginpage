f4e60507c1e46d0e5e9d8345dbad9cb507e92fb1
HARSH VARDHAN SINGH GAUR <91357841+gaurharsh@users.noreply.github.com>
2022-02-07 11:16:40 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHO-eEukw5j_7iomLySG9xmVSEtVbpQHY",
+  authDomain: "harrisonharsh-acc07.firebaseapp.com",
+  projectId: "harrisonharsh-acc07",
+  storageBucket: "harrisonharsh-acc07.appspot.com",
+  messagingSenderId: "491835015415",
+  appId: "1:491835015415:web:30f8d36ed0f9f9685ed3f2",
+  measurementId: "G-3PJ6H0VJ4Z"
+};
